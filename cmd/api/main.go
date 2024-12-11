750abfc878e772ac9c72e3370a5a8a3ddef88fbc
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-12-11 07:48:15 +0500

    Implement basic CRUD ops

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/itstnslv/greenlight/internal/data"
 	"log/slog"
 	"net/http"
 	"os"
@@ -29,6 +30,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -60,6 +62,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
